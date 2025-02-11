391ceaa25f34e435d064be509143b14ce48990f5
Walkername <05Timur51@gmail.com>
2025-02-11 22:43:19 +0300

    feat(auth): added security-filter, jwt-filter, cors, token validation

---
+++ src/main/java/ru/walkername/rating_system/services/TokenService.java
@@ -0,0 +1,26 @@
+package ru.walkername.rating_system.services;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TokenService {
+
+    @Value("${auth.jwt.secret}")
+    private String secret;
+
+    public DecodedJWT validateToken(String token) throws JWTVerificationException {
+        JWTVerifier verifier = JWT.require(Algorithm.HMAC256(secret))
+                .withSubject("User details")
+                .withIssuer("auth-service")
+                .build();
+
+        return verifier.verify(token);
+    }
+
+}
